dca59b7e2e11d19cb6826a9fb94aa8623cd310b4
jp112sdl <jp112sdl@icloud.com>
2018-07-03 08:37:20 +0200

    fixed chmod for (un)install files

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ ADDON_NAME=jp-hb-devices
 rm ${ADDON_NAME}-addon.tgz
 cd src
 chmod +x update_script
-chmod +x addon/install
-chmod +x addon/uninstall
+chmod +x addon/install_*
+chmod +x addon/uninstall_*
 chmod +x addon/update-check.cgi
 chmod +x rc.d/*
 find . -name ".DS_Store" -exec rm -rf {} \;

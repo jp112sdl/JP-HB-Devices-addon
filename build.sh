3aa875b739ad9952e76bebdb55188a8a9533bf08
jp112sdl <jp112sdl@icloud.com>
2018-12-02 11:21:51 +0100

    make build.sh more verbose

--- build.sh
+++ build.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 COPYFILE_DISABLE=1; export COPYFILE_DISABLE
 ADDON_NAME=jp-hb-devices
+
 wget -q -O ./patchsource/www/webui/webui.js.orig https://github.com/eq-3/occu/raw/master/WebUI/www/webui/webui.js
+echo "wget webui.js      exited with $?"
 wget -q -O ./patchsource/www/rega/esp/functions.fn.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/esp/functions.fn
+echo "wget functions.fn  exited with $?"
 wget -q -O ./patchsource/www/rega/esp/side.inc.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/esp/side.inc
+echo "wget side.inc      exited with $?"
 wget -q -O ./patchsource/www/rega/pages/tabs/admin/views/programs.htm.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/pages/tabs/admin/views/programs.htm
+echo "wget programs.htm  exited with $?"
+wget -q -O ./patchsource/www/config/ic_common.tcl.orig https://raw.githubusercontent.com/jens-maus/RaspberryMatic/master/buildroot-external/patches/occu/0000-CCU-Firmware-Diff/occu/WebUI/www/config/ic_common.tcl
+echo "wget ic_common.tcl exited with $?"
 
 diff -Naur ./patchsource/www/rega/esp/side.inc.orig ./patchsource/www/rega/esp/side.inc > ./src/addon/patch/hb-dis-ep-42bw.patch 
 diff -Naur ./patchsource/www/rega/esp/functions.fn.orig ./patchsource/www/rega/esp/functions.fn >> ./src/addon/patch/hb-dis-ep-42bw.patch 
 diff -Naur ./patchsource/www/webui/webui.js.orig ./patchsource/www/webui/webui.js >> ./src/addon/patch/hb-dis-ep-42bw.patch 
 diff -Naur ./patchsource/www/rega/pages/tabs/admin/views/programs.htm.orig ./patchsource/www/rega/pages/tabs/admin/views/programs.htm >> ./src/addon/patch/hb-dis-ep-42bw.patch 
+diff -Naur ./patchsource/www/config/ic_common.tcl.orig ./patchsource/www/config/ic_common.tcl >> ./src/addon/patch/hb-dis-ep-42bw.patch 
+echo "Patch created."
 
 rm ${ADDON_NAME}-addon.tgz
 cd src
@@ -21,5 +30,6 @@ chmod +x rc.d/*
 find . -name ".DS_Store" -exec rm -rf {} \;
 find . -name "._*" -exec rm -rf {} \;
 dot_clean .
-tar -zcvf ../${ADDON_NAME}-addon.tgz *
+tar --exclude='untitled.txt' -zcf ../${ADDON_NAME}-addon.tgz * 
+echo "tar created."
 cd ..

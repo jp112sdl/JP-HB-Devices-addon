2a205b5b332bc115618825622317a0dfaef20c96
jp112sdl <jp112sdl@icloud.com>
2018-12-09 19:13:34 +0100

    don't load original files before creating patch file

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@ COPYFILE_DISABLE=1; export COPYFILE_DISABLE
 ADDON_NAME=jp-hb-devices
 
 #wget -q -O ./patchsource/www/webui/webui.js.orig https://github.com/eq-3/occu/raw/master/WebUI/www/webui/webui.js
-wget -q -O ./patchsource/www/webui/webui.js.orig https://github.com/eq-3/occu/raw/b_3_41/WebUI/www/webui/webui.js
-echo "wget webui.js      exited with $?"
-wget -q -O ./patchsource/www/rega/esp/functions.fn.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/esp/functions.fn
-echo "wget functions.fn  exited with $?"
-wget -q -O ./patchsource/www/rega/esp/side.inc.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/esp/side.inc
-echo "wget side.inc      exited with $?"
-wget -q -O ./patchsource/www/rega/pages/tabs/admin/views/programs.htm.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/pages/tabs/admin/views/programs.htm
-echo "wget programs.htm  exited with $?"
-wget -q -O ./patchsource/www/config/ic_common.tcl.orig https://raw.githubusercontent.com/jens-maus/RaspberryMatic/master/buildroot-external/patches/occu/0000-CCU-Firmware-Diff/occu/WebUI/www/config/ic_common.tcl
-echo "wget ic_common.tcl exited with $?"
+#wget -q -O ./patchsource/www/webui/webui.js.orig https://github.com/eq-3/occu/raw/b_3_41/WebUI/www/webui/webui.js
+#echo "wget webui.js      exited with $?"
+#wget -q -O ./patchsource/www/rega/esp/functions.fn.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/esp/functions.fn
+#echo "wget functions.fn  exited with $?"
+#wget -q -O ./patchsource/www/rega/esp/side.inc.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/esp/side.inc
+#echo "wget side.inc      exited with $?"
+#wget -q -O ./patchsource/www/rega/pages/tabs/admin/views/programs.htm.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/pages/tabs/admin/views/programs.htm
+#echo "wget programs.htm  exited with $?"
+#wget -q -O ./patchsource/www/config/ic_common.tcl.orig https://raw.githubusercontent.com/jens-maus/RaspberryMatic/master/buildroot-external/patches/occu/0000-CCU-Firmware-Diff/occu/WebUI/www/config/ic_common.tcl
+#echo "wget ic_common.tcl exited with $?"
 
 diff -Naur ./patchsource/www/rega/esp/side.inc.orig ./patchsource/www/rega/esp/side.inc > ./src/addon/patch/hb-dis-ep-42bw.patch 
 diff -Naur ./patchsource/www/rega/esp/functions.fn.orig ./patchsource/www/rega/esp/functions.fn >> ./src/addon/patch/hb-dis-ep-42bw.patch 

19f2688a5ce3027457b62656dd9054d1628e75fd
jp112sdl <jp112sdl@icloud.com>
2019-04-19 22:11:29 +0200

    HB-DIS-EP-42BW: add parameter "DISPLAY_REFRESH_WAIT_TIME"

---
+++ src/addon/www/config/easymodes/MASTER_LANG/HB-DIS-EP-42BW_HELP.js
@@ -0,0 +1,17 @@
+jQuery.extend(true,langJSON, {
+  "de" : {
+    "HelpTitle" : "Hilfe",
+    "HB_DISPLAY_REFRESH_WAIT_TIME" :
+      "Wartezeit bis zum Starten des ePaper-Refreshs, nachdem ein Telegramm empfangen wurde.<br/><br/>" +
+      "<i>Hinweis:<br/>"+
+      "W&auml;hrend des Refreshs ist das gesamte Ger&auml;t mehrere Sekunden blockiert und kann keine Funktelegramme senden/empfangen.<br/><br/>"+
+      "Sollen z.B. mehrere Informationen nacheinander/aufgesplittet an das Ger&auml;t gesendet werden (z.B. wegen &Uuml;berschreitung der max. Telegramml&auml;nge"+
+      "je &Uuml;bertragung), empfiehlt sich eine Einstellung von <u>mind. 5 Sekunden</u>.</i>",
+  },
+  "en" :{
+    "HelpTitle" : "Help",
+
+    "HB_DISPLAY_REFRESH_WAIT_TIME" :
+      "Insert translation here...",
+  }
+});

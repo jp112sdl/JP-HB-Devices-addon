4cd299efb51da51c902156178688fa1faf105398
jp112sdl <jp112sdl@icloud.com>
2019-04-22 11:09:01 +0200

    modified help text

--- src/addon/www/config/easymodes/MASTER_LANG/HB-DIS-EP-42BW_HELP.js
+++ src/addon/www/config/easymodes/MASTER_LANG/HB-DIS-EP-42BW_HELP.js
@@ -2,7 +2,9 @@ jQuery.extend(true,langJSON, {
   "de" : {
     "HelpTitle" : "Hilfe",
     "HB_DISPLAY_REFRESH_WAIT_TIME" :
-      "Wartezeit bis zum Starten des ePaper-Refreshs, nachdem ein Telegramm empfangen wurde.<br/><br/>" +
+      "Wartezeit bis zum Starten des ePaper-Refreshs, nachdem ein Telegramm empfangen wurde. " +
+      "Wird w&auml;hrenddessen ein weiteres Telegramm empfangen, beginnt sie erneut von vorn.<br/>" +
+      "W&auml;hrend der Wartezeit blinkt die Ger&auml;te-LED rot.<br/><br/>"+
       "<i>Hinweis:<br/>"+
       "W&auml;hrend des Refreshs ist das gesamte Ger&auml;t mehrere Sekunden blockiert und kann keine Funktelegramme senden/empfangen.<br/><br/>"+
       "Sollen z.B. mehrere Informationen nacheinander/aufgesplittet an das Ger&auml;t gesendet werden (z.B. wegen &Uuml;berschreitung der max. Telegramml&auml;nge"+

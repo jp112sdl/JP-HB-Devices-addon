ef61f911c1dd53f8307078157cac07862383356c
jp112sdl <jp112sdl@icloud.com>
2019-07-19 07:14:35 +0200

    removed timestamp in patch

--- build.sh
+++ build.sh
@@ -2,29 +2,21 @@
 COPYFILE_DISABLE=1; export COPYFILE_DISABLE
 ADDON_NAME=jp-hb-devices
 
-#wget -q -O ./patchsource/www/webui/webui.js.orig https://github.com/eq-3/occu/raw/master/WebUI/www/webui/webui.js
-#wget -q -O ./patchsource/www/webui/webui.js.orig https://github.com/eq-3/occu/raw/b_3_41/WebUI/www/webui/webui.js
-#echo "wget webui.js      exited with $?"
-#wget -q -O ./patchsource/www/rega/esp/functions.fn.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/esp/functions.fn
-#echo "wget functions.fn  exited with $?"
-#wget -q -O ./patchsource/www/rega/esp/side.inc.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/esp/side.inc
-#echo "wget side.inc      exited with $?"
-#wget -q -O ./patchsource/www/rega/pages/tabs/admin/views/programs.htm.orig https://raw.githubusercontent.com/eq-3/occu/master/WebUI/www/rega/pages/tabs/admin/views/programs.htm
-#echo "wget programs.htm  exited with $?"
-#wget -q -O ./patchsource/www/config/ic_common.tcl.orig https://raw.githubusercontent.com/jens-maus/RaspberryMatic/master/buildroot-external/patches/occu/0000-CCU-Firmware-Diff/occu/WebUI/www/config/ic_common.tcl
-#echo "wget ic_common.tcl exited with $?"
+file=./patchsource/www/rega/esp/functions.fn
+diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+file=./patchsource/www/webui/webui.js
+diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+file=./patchsource/www/config/ic_common.tcl
+diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
+file=./patchsource/www/rega/esp/datapointconfigurator.fn
+diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/common/`basename ${file}`.patch
 
-diff -Naur ./patchsource/www/rega/esp/functions.fn.orig ./patchsource/www/rega/esp/functions.fn > ./src/addon/patch/common/functions.fn.patch 
-diff -Naur ./patchsource/www/webui/webui.js.orig ./patchsource/www/webui/webui.js > ./src/addon/patch/common/webui.js.patch 
-diff -Naur ./patchsource/www/config/ic_common.tcl.orig ./patchsource/www/config/ic_common.tcl > ./src/addon/patch/common/ic_common.tcl.patch 
-diff -Naur ./patchsource/www/rega/esp/datapointconfigurator.fn.orig ./patchsource/www/rega/esp/datapointconfigurator.fn > ./src/addon/patch/common/datapointconfigurator.fn.patch 
-diff -Naur ./patchsource/www/rega/esp/side.inc.orig ./patchsource/www/rega/esp/side.inc > ./src/addon/patch/le_343/side.inc.patch 
-diff -Naur ./patchsource/www/rega/pages/tabs/admin/views/programs.htm.orig ./patchsource/www/rega/pages/tabs/admin/views/programs.htm > ./src/addon/patch/le_343/programs.htm.patch 
-
-diff -Naur ./patchsource/www/rega/esp/side.inc.orig-3.45 ./patchsource/www/rega/esp/side.inc-3.45 > ./src/addon/patch/ge_345/side.inc.patch 
-sed -i '' -e "s/-3.45//g" ./src/addon/patch/ge_345/side.inc.patch 
-diff -Naur ./patchsource/www/rega/pages/tabs/admin/views/programs.htm.orig-3.45 ./patchsource/www/rega/pages/tabs/admin/views/programs.htm-3.45 > ./src/addon/patch/ge_345/programs.htm.patch 
-sed -i '' -e "s/-3.45//g" ./src/addon/patch/ge_345/programs.htm.patch 
+file=./patchsource/www/rega/pages/tabs/admin/views/programs.htm
+diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/le_343/`basename ${file}`.patch 
+diff -u --label=${file}.orig --label=${file} ${file}.orig-3.45 ${file}-3.45 > ./src/addon/patch/ge_345/`basename ${file}`.patch
+file=./patchsource/www/rega/esp/side.inc
+diff -u --label=${file}.orig --label=${file} ${file}.orig ${file} > ./src/addon/patch/le_343/`basename ${file}`.patch 
+diff -u --label=${file}.orig --label=${file} ${file}.orig-3.45 ${file}-3.45 > ./src/addon/patch/ge_345/`basename ${file}`.patch
 
 echo "Patch created."
 

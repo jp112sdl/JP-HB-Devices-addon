ea11d563b2e1f15981fd5de43424319b0fbd54c0
jp112sdl <jp112sdl@icloud.com>
2019-07-26 18:18:14 +0200

    fixed box size

--- src/addon/www/config/easymodes/HB-DIS-EP-42BW_dev_master.tcl
+++ src/addon/www/config/easymodes/HB-DIS-EP-42BW_dev_master.tcl
@@ -10,7 +10,7 @@ proc getHelpIcon {topic x y} {
 }
 
 proc getIconOverviewIcon {topic x y} {
-  set ret "<img src=\"/ise/img/visible.png\" style=\"cursor: pointer; width:18px; height:18px; position:relative; top:2px\" onclick=\"showParamHelp('$topic', '410', '520')\">"
+  set ret "<img src=\"/ise/img/visible.png\" style=\"cursor: pointer; width:18px; height:18px; position:relative; top:2px\" onclick=\"showParamHelp('$topic', '$x', '210')\">"
   return $ret
 }
 

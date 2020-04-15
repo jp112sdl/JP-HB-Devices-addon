8325776f3158343139e1ce7e2d855e2d51ee594b
jp112sdl <jp112sdl@icloud.com>
2020-04-15 22:20:55 +0200

    add HB-UNI-Sen-LDR-SC

---
+++ src/addon/www/pda/eq3/controls/ldrsc.tcl
@@ -0,0 +1,27 @@
+#!/bin/tclsh
+
+set DPControls_GETSTATUS(LDRSC.STATE) [list LDRSCControl_getStatus]
+
+proc LDRSCControl_getStatus { channelId dataPoint } {
+	array set dp $dataPoint
+	
+	set    script "var dpId = \"$dp(ID)\";"
+	append script {
+		var dp = dom.GetObject(dpId);
+		Write(dp.Value());
+	}
+	
+	switch -exact -- [rega_exec $script] {
+		false		{ set status "Kein Licht erkannt"}
+		true		{ set status "Licht erkannt"}
+		default	{ set status "unbekannt" }
+	}
+	
+	set    result [status_separator]
+	append result {<tr><td class="status">}
+	append result {<div class="caption">Status</div>}
+	append result "<div class=\"center\">$status</div>"
+	append result {</td></tr>}
+	return $result
+
+}

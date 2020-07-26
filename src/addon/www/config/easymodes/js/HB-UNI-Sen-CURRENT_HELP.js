ee15640a82fc5aaee30fe534376f9bf0aecf98ab
jp112sdl <jp112sdl@icloud.com>
2020-07-26 21:59:29 +0200

    fix help text for datapoint

--- src/addon/www/config/easymodes/js/HB-UNI-Sen-CURRENT_HELP.js
+++ src/addon/www/config/easymodes/js/HB-UNI-Sen-CURRENT_HELP.js
@@ -1,14 +1,14 @@
 jQuery.extend(true,langJSON, {
   "de" : {
     "HelpTitle" : "Information",
-    "HB_MEASURE_INTERVAL" :
+    "HBWEA_TRANSMIT_INTERVAL" :
       "Es wird der Mittelwert &uuml;ber die <i>Sendeintervall</i> Anzahl Messungen gebildet und &uuml;bertragen.<br/>Der zeitliche Sendeabstand betr&auml;gt (Messintervall x Sendeintervall) Sekunden",
     "BACKLIGHT_ON_TIME"  :
       "0 = LCD Hintergrundbeleuchtung dauerhaft EIN"
   },
   "en" :{
     "HelpTitle" : "Information",
-    "HB_MEASURE_INTERVAL" :
+    "HBWEA_TRANSMIT_INTERVAL" :
       "not translated",
     "BACKLIGHT_ON_TIME"  :
       "0 = lcd backlight permanent ON"  

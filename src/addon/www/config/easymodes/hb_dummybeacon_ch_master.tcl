bdfbb2d46cad20152b490ce5e6efb7c41333d596
jp112sdl <jp112sdl@icloud.com>
2020-12-02 17:26:30 +0100

    add HB-UNI-Sen-DUMMY-BEACON-V2

---
+++ src/addon/www/config/easymodes/hb_dummybeacon_ch_master.tcl
@@ -0,0 +1,169 @@
+#!/bin/tclsh
+source [file join $env(DOCUMENT_ROOT) config/easymodes/em_common.tcl]
+source [file join $env(DOCUMENT_ROOT) config/easymodes/EnterFreeValue.tcl]
+
+proc getMinValue {ps_descr param} {
+  upvar ps_descr descr
+  array_clear param_descr
+  array set param_descr $descr($param)
+  return $param_descr(MIN)
+}
+
+proc getMaxValue {ps_descr param} {
+  upvar ps_descr descr
+  array_clear param_descr
+  array set param_descr $descr($param)
+  return $param_descr(MAX)
+}
+
+proc getMinMaxValueDescr {ps_descr param} {
+  upvar ps_descr descr 
+  array_clear param_descr
+  array set param_descr $descr($param)
+  set min $param_descr(MIN)
+  set max $param_descr(MAX)
+
+  return "($min - $max)" 
+}
+
+proc getUnit {ps_descr param} {
+  upvar ps_descr descr 
+  array_clear param_descr
+  array set param_descr $descr($param)
+  set unit $param_descr(UNIT)
+  return "$unit"
+}
+
+proc getCheckBox {param value chn prn} {
+  set checked ""
+  if { $value } then { set checked "checked=\"checked\"" }
+  set s "<input id='separate_CHANNEL\_$chn\_$prn' type='checkbox' $checked value='dummy' name=$param>"
+  return $s
+}
+
+proc get_devicenames {allDevices dummydeviceAddress} {
+  global ise_CHANNELNAMES iface_url iface
+  
+  set url $iface_url($iface)
+  
+  set dummydeviceAddr [lindex [split $dummydeviceAddress ":"] 0]  
+  set _iface [string tolower $iface]
+  array_clear result 
+
+  foreach val $allDevices {
+    array set devDescr $val
+    if {($devDescr(ADDRESS) != $dummydeviceAddr ) &&  ($devDescr(PARENT) == "") } {
+      set name [string tolower $ise_CHANNELNAMES($iface;$devDescr(ADDRESS))]   
+      if { [string first $_iface $name] == -1} {
+        set result($devDescr(ADDRESS)) $ise_CHANNELNAMES($iface;$devDescr(ADDRESS))
+      }
+    }
+  }
+  return [array get result]
+}
+
+proc get_devname { address } {  
+  set rega_cmd ""
+  append rega_cmd "string devName = '$address';"
+  append rega_cmd "string dev_id;"
+  append rega_cmd "foreach(dev_id, dom.GetObject(ID_DEVICES).EnumUsedIDs()){"
+  append rega_cmd "object dev=dom.GetObject(dev_id);"
+  append rega_cmd "if (dev.Address() == devName) { devName = dev.Name(); }"
+  append rega_cmd "}"
+  
+  array set result [rega_script $rega_cmd]
+  return $result(devName);
+}
+
+proc set_htmlParams {iface address pps pps_descr special_input_id peer_type} {
+  puts "<script type=\"text/javascript\">load_JSFunc('/config/easymodes/js/DummyBeacon.js');</script>"
+
+  global iface_url
+
+  upvar PROFILES_MAP  PROFILES_MAP
+  upvar HTML_PARAMS   HTML_PARAMS
+  upvar PROFILE_PNAME PROFILE_PNAME
+  upvar $pps          ps
+  upvar $pps_descr  ps_descr
+  
+  set url $iface_url($iface)
+
+  set comment {
+    foreach val [array names ps] {
+      puts "$val: $ps($val)<br/>"
+    }
+    foreach val [array names ps_descr] {
+      puts "$val: $ps_descr($val)<br/>"
+    }
+
+    puts "<br/><br/>"
+  }
+
+  set allDevices [xmlrpc $url listDevices]
+  set chn [lindex [split $special_input_id _] 1]                                                                                              
+  set paramCount 0
+  set senderRF "" 
+  array_clear devnames
+  array set devnames [get_devicenames $allDevices $address] 
+  array set ccuDescr [xmlrpc $url getDeviceDescription [list string "BidCoS-RF"]]
+  
+  append HTML_PARAMS(separate_1) "<table class=\"ProfileTbl\">"
+
+  append HTML_PARAMS(separate_1) "<tr>"
+  append HTML_PARAMS(separate_1) "<td class=\"stringtable_value\">\${stringTableFdSimulate}</td>"
+  append HTML_PARAMS(separate_1) "<td><select class=\"select\_$chn\" id=\"deviceSelectionBox\_$chn\" onchange=\"setDevice(this, $chn)\">"
+  append HTML_PARAMS(separate_1) "<option id='$chn\_0' value='0'>-&nbsp;nicht&nbsp;belegt&nbsp;-</option>"
+
+  foreach name [array names devnames] {
+    array set parentDescr [xmlrpc $url getDeviceDescription [list string $name]]
+    set senderRF     $parentDescr(RF_ADDRESS)
+    set senderSERIAL $parentDescr(ADDRESS)
+    set senderNAME   [get_devname $senderSERIAL]
+    if {$senderRF != $ccuDescr(RF_ADDRESS)} {   
+      append HTML_PARAMS(separate_1) "<option id='$chn\_$senderRF' value='$senderRF'>$senderNAME ($senderSERIAL)</option>"
+    }
+  }
+  append HTML_PARAMS(separate_1) "</select></td>"
+    append HTML_PARAMS(separate_1) "<td class=\"HEXADDR\_$chn j_param_$chn\"></td>"
+  append HTML_PARAMS(separate_1) "</tr>"
+
+  incr paramCount
+  set param "ADDRESS_SENDER_HIGH_BYTE"
+  append HTML_PARAMS(separate_1) "<input class=\"$param\_$chn\" type=\"text\" id=\"separate_CHANNEL_$chn\_$paramCount\" name=\"$param\" value=\"$ps($param)\" >"
+
+  incr paramCount
+  set param "ADDRESS_SENDER_MID_BYTE"
+  append HTML_PARAMS(separate_1) "<input class=\"$param\_$chn\" type=\"text\" id=\"separate_CHANNEL_$chn\_$paramCount\" name=\"$param\" value=\"$ps($param)\" >"
+
+  incr paramCount
+  set param "ADDRESS_SENDER_LOW_BYTE"
+  append HTML_PARAMS(separate_1) "<input class=\"$param\_$chn\" type=\"text\" id=\"separate_CHANNEL_$chn\_$paramCount\" name=\"$param\" value=\"$ps($param)\" >"
+
+  incr paramCount
+  set param "FD_CYCLIC_TIMEOUT"
+  append HTML_PARAMS(separate_1) "<tr class=\"j_param_$chn\">"
+    append HTML_PARAMS(separate_1) "<td class=\"j_param_$chn stringtable_value\">\${stringTableFdSendEvery}</td>"
+    append HTML_PARAMS(separate_1) "<td class=\"j_param_$chn\"><input type=\"text\" style=\"width: 64px;\" id=\"separate_CHANNEL_$chn\_$paramCount\" name=\"$param\" value=\"$ps($param)\" onblur=\"ProofAndSetValue('separate_CHANNEL_$chn\_$paramCount','separate_CHANNEL_$chn\_$paramCount', parseInt([getMinValue ps_descr $param]), parseInt([getMaxValue ps_descr $param]), parseFloat(1));\"> </td>"
+    append HTML_PARAMS(separate_1) "<td class=\"j_param_$chn\">[getUnit ps_descr $param] <i>[getMinMaxValueDescr ps_descr $param]</i></td>"
+  append HTML_PARAMS(separate_1) "</tr>"
+    
+  incr paramCount
+  set param "FD_STATUS"
+  append HTML_PARAMS(separate_1) "<tr class=\"j_param_$chn\">"
+    append HTML_PARAMS(separate_1) "<td class=\"j_param_$chn stringtable_value\">den \${stringTableFdStatus}</td>"
+    append HTML_PARAMS(separate_1) "<td class=\"j_param_$chn\"> <input type=\"text\" style=\"width: 40px;\" id=\"separate_CHANNEL_$chn\_$paramCount\" name=\"$param\" value=\"$ps($param)\" onblur=\"ProofAndSetValue('separate_CHANNEL_$chn\_$paramCount','separate_CHANNEL_$chn\_$paramCount', parseInt([getMinValue ps_descr $param]), parseInt([getMaxValue ps_descr $param]), parseFloat(1));\"> </td>"
+    append HTML_PARAMS(separate_1) "<td class=\"j_param_$chn\">[getUnit ps_descr $param] <i>[getMinMaxValueDescr ps_descr $param]</i></td>"
+  append HTML_PARAMS(separate_1) "</tr>"
+  
+  incr paramCount
+  set param "FD_BROADCAST"
+  append HTML_PARAMS(separate_1) "<tr class=\"j_param_$chn\">"
+    append HTML_PARAMS(separate_1) "<td class=\"j_param_$chn stringtable_value\">\${stringTableFdBroadcast}</td>"
+    append HTML_PARAMS(separate_1) "<td class=\"j_param_$chn\">[getCheckBox '$param' $ps($param) $chn $paramCount]</td>"
+  append HTML_PARAMS(separate_1) "</tr>"
+    
+  append HTML_PARAMS(separate_1) "</table>"
+  
+  puts "<script type=\"text/javascript\">init($chn);</script>"
+}
+constructor
---
+++ src/addon/www/config/easymodes/js/DummyBeacon.js
@@ -0,0 +1,108 @@
+convertAddress = function(address) {
+  var val = {};
+  if (address) {  
+    var hAddress = parseInt(address).toString(16);
+  
+    while (hAddress.length < 6) {
+      hAddress = "0" + hAddress;
+    }
+    
+    val.LOW = parseInt(hAddress.substring(4), 16).toString(); 
+    val.LOW = (isNaN(val.LOW)) ? "0" : val.LOW.toString();
+    val.MID = parseInt(hAddress.substring(2,4), 16).toString();
+    val.MID = (isNaN(val.MID)) ? "0" : val.MID;
+    val.HIGH = parseInt(hAddress.substring(0,2), 16);
+    val.HIGH = (isNaN(val.HIGH)) ? "0" : val.HIGH.toString();
+  } else {
+    val.LOW = "0";
+    val.MID = "0";
+    val.HIGH = "0";
+  }
+  return val;
+};
+
+restoreRFAddress = function(oVal) {
+  
+  var hexSenderAddress = "000000";
+
+  var hSenderAddressHIGH = parseInt(oVal.senderHIGH).toString(16);
+  hSenderAddressHIGH = (hSenderAddressHIGH.length == 1) ? "0" +  hSenderAddressHIGH : hSenderAddressHIGH;
+
+  var hSenderAddressMID = parseInt(oVal.senderMID).toString(16); 
+  hSenderAddressMID = (hSenderAddressMID.length == 1) ? "0" +  hSenderAddressMID : hSenderAddressMID;
+
+  var hSenderAddressLOW = parseInt(oVal.senderLOW).toString(16); 
+  hSenderAddressLOW = (hSenderAddressLOW.length == 1) ? "0" +  hSenderAddressLOW : hSenderAddressLOW;
+
+  hexSenderAddress = hSenderAddressHIGH + hSenderAddressMID + hSenderAddressLOW;
+   
+  return parseInt(hexSenderAddress, 16);
+};
+
+decToHex = function(val) {
+  var sHex = parseInt(val).toString(16);
+  return (sHex.length == 1) ? "0" +  sHex : sHex;
+}
+
+self.init = function (chn) {
+  var options = jQuery(".select_"+chn+" option");
+  if (options) {
+    var arr = options.map(function(_, o) {
+        return {
+            t: jQuery(o).text(),
+            v: o.value,
+            i: o.id
+        };
+    }).get();
+    arr.sort(function(o1, o2) {
+        return o1.t > o2.t ? 1 : o1.t < o2.t ? -1 : 0;
+    });
+    options.each(function(j, o) {
+        //console.log(j);
+        o.value = arr[j].v;
+        o.id = arr[j].i;
+        jQuery(o).text(arr[j].t);
+    });
+  }
+    
+  jQuery(".ADDRESS_SENDER_HIGH_BYTE_"+chn).hide();
+  jQuery(".ADDRESS_SENDER_MID_BYTE_"+chn).hide();
+  jQuery(".ADDRESS_SENDER_LOW_BYTE_"+chn).hide();
+  
+  var high = jQuery(".ADDRESS_SENDER_HIGH_BYTE_"+chn).val();
+  var mid = jQuery(".ADDRESS_SENDER_MID_BYTE_"+chn).val();
+  var low = jQuery(".ADDRESS_SENDER_LOW_BYTE_"+chn).val();
+
+  var val = {};
+  val.senderHIGH=high;
+  val.senderMID=mid;
+  val.senderLOW=low;
+
+  var rf = restoreRFAddress(val);
+  document.getElementById("deviceSelectionBox_"+chn).value = rf;
+  
+  setDevice(document.getElementById("deviceSelectionBox_"+chn), chn);
+} 
+
+
+self.setDevice = function (elm, chn) {
+  console.log("setDevice "+elm.value+" ch:"+chn);
+
+  if (elm.value === "0") {
+    jQuery(".j_param_"+chn).hide();
+    jQuery(".ADDRESS_SENDER_HIGH_BYTE_"+chn).val(0);
+    jQuery(".ADDRESS_SENDER_MID_BYTE_"+chn).val(0); 
+    jQuery(".ADDRESS_SENDER_LOW_BYTE_"+chn).val(0); 
+  } else {
+    jQuery(".j_param_"+chn).show();
+    var senderBYTES = convertAddress(parseInt(elm.value));
+    jQuery(".ADDRESS_SENDER_HIGH_BYTE_"+chn).val(senderBYTES.HIGH); 
+    jQuery(".ADDRESS_SENDER_MID_BYTE_"+chn).val(senderBYTES.MID); 
+    jQuery(".ADDRESS_SENDER_LOW_BYTE_"+chn).val(senderBYTES.LOW); 
+    
+    jQuery(".HEXADDR_"+chn).html("(0x "+decToHex(senderBYTES.HIGH)+" "+decToHex(senderBYTES.MID)+" "+decToHex(senderBYTES.LOW)+")"); 
+  }
+
+};
+
+

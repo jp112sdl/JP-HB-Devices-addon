b32b95b16f1f158d20cf757fbe0ceebadfd04da9
jp112sdl <jp112sdl@icloud.com>
2021-05-25 21:59:35 +0200

    HB-OU-RGBW-LED-FX: - re-ordered items - add some help icons

---
+++ src/addon/www/config/easymodes/MASTER_LANG/HB-OU-RGBW-LED-FX_HELP.js
@@ -0,0 +1,14 @@
+jQuery.extend(true,langJSON, {
+  "de" : {
+    "HelpTitle" : "Hilfe",
+    "SHORT_ACT_OPTIONS" : "Optionen sind wie folgt zu berechnen: <a href='https://github.com/kitesurfer1404/WS2812FX/blob/master/extras/WS2812FX%20Users%20Guide.md#even-more-about-segments' target='_blank' rel='noopener noreferrer'> siehe WS2812FX Users Guide</a>",
+    "SHORT_ACT_INTENS" : "Die Helligkeit bezieht sich auf den gesamten Stripe"
+  },
+
+  "en" : {
+    "HelpTitle" : "Help",
+    "SHORT_ACT_OPTIONS" : "how to calculate options: <a href='https://github.com/kitesurfer1404/WS2812FX/blob/master/extras/WS2812FX%20Users%20Guide.md#even-more-about-segments' target='_blank' rel='noopener noreferrer'> see WS2812FX Users Guide</a>",
+    "SHORT_ACT_INTENS" : "brightness is for the whole stripe"
+  }
+});
+
